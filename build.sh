4d47a9d69a3613ba68e3a5b003606f9b3135b3f5
Prosecuter <anuragbajirao@gmail.com>
2020-08-27 21:25:31 +0530

    Bump revision
    Signed-off-by: Prosecuter <anuragbajirao@gmail.com>

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ export KBUILD_BUILD_USER="StormbreakerCI-BOT"
 export GCC_COMPILE="$GCC" 
 export CLANG_VER="$VER_CLANG"
 export KBUILD_BUILD_HOST="Stormbreaker-HQ"
-export REVISION="7.1"
+export REVISION="7.32"
 
 # Export credentials via a secret because why not
 echo "$GITHUB_CREDENTIAL" > $HOME/.git-credentials
