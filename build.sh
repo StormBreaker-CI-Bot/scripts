ef1d3267a830f7ee631ec2c87fed0954cf6b012e
Prosecuter <anuragbajirao@gmail.com>
2020-08-28 03:48:41 +0530

    Ready for release
    Signed-off-by: Prosecuter <anuragbajirao@gmail.com>

--- build.sh
+++ build.sh
@@ -90,7 +90,7 @@ rm -rf $ZIP_DIR && git clone https://github.com/Prosecuter/AnyKernel3 $ZIP_DIR
 function clone_kernel(){
 
 mkdir -p $KERNEL_DIR
-git clone --depth=1 https://${GITHUB_USER}@github.com/stormbreaker-project/vinc2 -b dev/pelt $KERNEL_DIR
+git clone --depth=1 https://${GITHUB_USER}@github.com/stormbreaker-project/vince -b main $KERNEL_DIR
 cd $KERNEL_DIR
 
 }
